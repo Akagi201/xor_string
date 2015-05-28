fcc62bd06b7d4137e049b54360a66c3f3d017fab
Akagi201 <akagi201@gmail.com>
2015-05-28 22:55:28 +0800

    initial version

---
+++ xor-string/xor_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from itertools import cycle, izip
+
+def xor_string(message, key):
+    cyphered = ''.join(chr(ord(c)^ord(k)) for c,k in izip(message, cycle(key)))
+    print('%s ^ %s = %s' % (message, key, cyphered))
+    return cyphered
+
+if __name__ == '__main__':
+    enc = xor_string('attack at dawn', 's3cr3t')
+    dec = xor_string(enc, 's3cr3t')
+    print(enc)
+    print(dec)
