f86a710f5b74f6ea5cf76d260097ecf855b992c3
Akagi201 <akagi201@gmail.com>
2015-11-02 14:35:58 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 
+import os
+
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
 
 requires = [
     'itertools',
@@ -12,7 +17,7 @@ setup(
     url='https://github.com/Akagi201/xor-string',
     download_url='https://github.com/Akagi201/xor-string/tarball/0.1.0',
     description='Elegant xor encryption in Python',
-    long_description=open('README.md').read(),
+    long_description=README,
     license='MIT',
     author='Akagi201',
     author_email='akagi201@gmail.com',
