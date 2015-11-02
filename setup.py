503e3894421a87c400d8bfc7d67f89176fd56bb0
Akagi201 <akagi201@gmail.com>
2015-11-02 20:37:19 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,50 @@
-
-# import os
+import os
 
 from setuptools import setup
 
-# here = os.path.abspath(os.path.dirname(__file__))
-# README = open(os.path.join(here, 'README.md')).read()
 
-requires = [
+def read_file(filename, encoding='utf8'):
+    """Read unicode from given file."""
+    with codecs.open(filename, encoding=encoding) as fd:
+        return fd.read()
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+readme = read_file(os.path.join(here, 'README.md'))
+
+install_requires = [
     'itertools',
 ]
 
 setup(
     name='xor_string',
-    packages=['xor_string',],
-    version='0.2.0',
+    packages=['xor_string', ],
+    version='0.2.1',
     url='https://github.com/Akagi201/xor_string',
-    download_url='https://github.com/Akagi201/xor_string/tarball/0.2.0',
+    download_url='https://github.com/Akagi201/xor_string/tarball/0.2.1',
     description='Elegant xor encryption in Python',
-    # long_description=README,
+    long_description=readme,
     license='MIT',
     author='Akagi201',
     author_email='akagi201@gmail.com',
     keywords=['xor', 'string', 'itertools', 'python'],
-    classifiers=[],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Security',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
 )
