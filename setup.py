1a0c61a76ce0ce67ec280f90cf820d85d9653fb6
Akagi201 <akagi201@gmail.com>
2015-12-27 16:11:46 +0800

    add rst long_description

--- setup.py
+++ setup.py
@@ -8,20 +8,29 @@ def read_file(filename, encoding='utf8'):
     """Read unicode from given file."""
     with codecs.open(filename, encoding=encoding) as fd:
         return fd.read()
+try:
+    import pandoc
+    pandoc.core.PANDOC_PATH = '/usr/local/bin/pandoc'
+    doc = pandoc.Document()
+    here = os.path.abspath(os.path.dirname(__file__))
+    # doc.markdown = read_file(os.path.join(here, 'README.md'))
+    doc.markdown = open('README.md').read()
+    ld = doc.rst
+except:
+    ld = 'See:\nhttps://github.com/Akagi201/xor_string\n'
 
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-readme = read_file(os.path.join(here, 'README.md'))
+# here = os.path.abspath(os.path.dirname(__file__))
+#
+# readme = read_file(os.path.join(here, 'README.md'))
 
 setup(
     name='xor_string',
     packages=['xor_string', ],
-    version='0.2.3',
+    version='0.2.4',
     url='https://github.com/Akagi201/xor_string',
-    download_url='https://github.com/Akagi201/xor_string/tarball/0.2.3',
+    download_url='https://github.com/Akagi201/xor_string/tarball/0.2.4',
     description='Elegant xor encryption in Python',
-    long_description=readme,
+    long_description=ld,
     license='MIT',
     author='Akagi201',
     author_email='akagi201@gmail.com',
